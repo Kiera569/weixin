eec884df9b7db65134156f6101344e562fc9a2ec
kiera569 <1053189514@qq.com>
2020-07-17 16:39:32 +0800

    fix person组件

--- src/app/home/component/appointment/index.js
+++ src/app/home/component/appointment/index.js
@@ -1,17 +1,58 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
-import './index.less'
+import { Tabs } from 'antd-mobile'
+import './index.less';
+import PersonInfo from '../../../../components/person-info'
 @withRouter
 class Appointment extends Component {
   gotoDoctor = () => {
     const { history } = this.props;
     history.push("/doctorDetail");
   };
+  renderContent = tab =>
+    (
+      <PersonInfo />
+    );
+
+  getInfo = (tabs) => {
+    tabs.map((item) => {
+      return <PersonInfo key={item.title} />
+    })
+  }
 
   render() {
-    console.log(this.props)
+    console.log(this.props.match.params)
+    const tabs = [
+      { title: '周一', date: '07.15' },
+      { title: '2nd Tab' },
+      { title: '3rd Tab' },
+      { title: '4th Tab' },
+      { title: '5th Tab' },
+      { title: '6th Tab' },
+      { title: '7th Tab' },
+      { title: '8th Tab' },
+      { title: '9th Tab' },
+    ];
     return (
-      <div>
+      <div className="preAppointment">
+        <div className="appoint-title">
+          <div className="departMent">泌尿科</div>
+          <div className="depart">甲状腺内分泌专科</div>
+        </div>
+        <div className="appoint-content">
+          <PersonInfo />
+          {/* <Tabs tabs={tabs} tabBarActiveTextColor="#29b9c3" tabBarUnderlineStyle={{ border: "2px solid #29b9c3" }} renderTabBar={props => <Tabs.DefaultTabBar {...props} page={3} onTabClick={() => this.getInfo(tabs)} />}>
+            {this.renderContent}
+          </Tabs> */}
+          {/* <div className="appoint-content-head">
+            <div className="content-tab">
+              <div>zhou</div>
+              <div>07.15</div>
+            </div>
+          </div>
+          <div className="appoint-content-info"></div> */}
+        </div>
+
 
       </div>
     );
--- src/components/person-info/index.js
+++ src/components/person-info/index.js
@@ -5,29 +5,30 @@ import "./index.less";
 @withRouter
 class PersonInfo extends Component {
   submit = () => {
-    // const { push } = this.props;
-    const { history } = this.props;
-    console.log(history);
-    history.push("/appointment");
+    console.log(111)
   };
   render() {
     return (
-      <div className="person-box">
-        <div className="person-img">
-          <img
-            src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1594360854144&di=6d9dabfe26d714b95a8e59edf5452673&imgtype=0&src=http%3A%2F%2Fdingyue.nosdn.127.net%2FLoW9vSuNRJO8cDpfEpJBkdLVtIyNoKw2On4OEfJt8lF9J1557269902362compressflag.jpeg"
-            alt=""
-          />
+      <div className="doctor-info">
+        <div className="person-box">
+          <div className="person-img">
+            <img
+              // src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1594360854144&di=6d9dabfe26d714b95a8e59edf5452673&imgtype=0&src=http%3A%2F%2Fdingyue.nosdn.127.net%2FLoW9vSuNRJO8cDpfEpJBkdLVtIyNoKw2On4OEfJt8lF9J1557269902362compressflag.jpeg"
+              alt=""
+            />
+          </div>
+          <div className="info-box">
+            <div className="info-title">
+              <span className="person-name">华佗</span>
+              <div className="person-level"> 主治医师</div>
+            </div>
+            <div className="person-info">亚专业:
+            <span className='person-info-text'>23242535s35</span>
+              <div className='pre-btn' onClick={this.submit}>预约</div>
+            </div>
+            <div className="person-tech">专业擅长:<span className='person-tech-text'>2sssssssssssssssssssssssdd3</span></div>
+          </div>
         </div>
-        <div className="info-box">
-          <span className="person-name">华佗</span>
-          <span className="person-level"> 主治医师</span>
-          <div className="person-info">介绍:22232</div>
-          <div className="person-tech">擅长技术:444444</div>
-        </div>
-        <Button type="primary" onClick={this.submit} className="pre-btn">
-          预约
-        </Button>
       </div>
     );
   }
