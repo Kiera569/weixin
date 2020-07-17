e04e5bc28164de7d202365746703d670d43d80cb
kiera569 <1053189514@qq.com>
2020-07-17 18:22:52 +0800

    fix bug

--- src/app/doctorDetail/index.js
+++ src/app/doctorDetail/index.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import PersonDetail from '../../components/person-detail'
+import { Accordion } from "antd-mobile";
 
 
 class doctorDetail extends Component {
+  onChange = (key) => {
+    console.log(key);
+  }
   render() {
     return (
       <div>
         <PersonDetail />
+        <div className="person-introduce">
+          <Accordion defaultActiveKey="0" className="my-accordion" onChange={this.onChange}>
+            <Accordion.Panel header="个人简介" className="pad">
+              333333333333
+            </Accordion.Panel>
+          </Accordion>
+
+
+        </div>
 
       </div>
     );
