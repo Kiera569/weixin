76cc405285aa95da8c5b2a9c2e2c8eba4f03dd0f
kiera569 <1053189514@qq.com>
2020-07-20 11:22:20 +0800

    医生详情

--- src/app/doctorDetail/index.js
+++ src/app/doctorDetail/index.js
@@ -1,12 +1,19 @@
 import React, { Component } from "react";
-import { Accordion } from "antd-mobile";
+import { Accordion, Button } from "antd-mobile";
 import PersonDetail from "../../components/person-detail";
 import "./index.less";
 
 class doctorDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isBtn: -1  // 是否点击了排班框
+    }
+  }
   onChange = key => {
     console.log(key);
   };
+
   renderHead = (a, b) => (
     <div>
       <span className="aaa">{b}</span>
@@ -14,9 +21,24 @@ class doctorDetail extends Component {
     </div>
   );
 
+
+  // 点击排班框
+  orderBtn = (item, index) => {
+    this.setState({
+      isBtn: index
+    })
+
+  }
+
+  // 确认预约 
+  confirmBtn = () => {
+    console.log(111)
+  }
+
   render() {
+    const { isBtn } = this.state;
     return (
-      <div>
+      <div className="doctor-detail">
         <PersonDetail />
         <div className="person-introduce">
           <Accordion
@@ -40,8 +62,7 @@ class doctorDetail extends Component {
             onChange={this.onChange}
           >
             <Accordion.Panel
-              //  header={this.personalInfo}
-              header="专业擅长"
+              header={this.renderHead("专业擅长", "icon")}
               className="pad "
             >
               甲状腺结节良恶性的鉴别；2，甲状腺及甲状旁腺常规,四川省中医学
@@ -54,9 +75,33 @@ class doctorDetail extends Component {
               评为市级有突出贡献的专家.
             </Accordion.Panel>
           </Accordion>
+
+          <div className="work-order">
+            {[1, 2, 3, 4, 5].map((item, index) => {
+              return (<WorkOrderBox text="07.15 星期三 上午" handleBtn={() => this.orderBtn(item, index)} index={index} isBtn={isBtn} key={item} isPre={true} />)
+
+            })
+            }
+
+            <Button className="confirm-pre" type="primary" onClick={this.confirmBtn}>确认预约</Button>
+          </div>
+
         </div>
+
       </div>
     );
   }
 }
 export default doctorDetail;
+
+
+function WorkOrderBox({ text, handleBtn, index, isBtn, isPre }) {
+  const HasNumStyle = isPre ? "order-info-active" : "order-info-unactive"; // 是否有号
+  // 是否选中当前号
+  const IsBtnStyle = isBtn === index ? "order-info-isbtn" : ""
+  return (
+    <div className={"order-info" + " " + `${HasNumStyle}` + " " + `${IsBtnStyle}`} onClick={() => handleBtn()}>
+      {text}
+    </div>
+  )
+}
